958d89ada5c482bf20e9061b04f322545726f623
Jymmy Murillo <murillojymmy@gmail.com>
2022-04-07 16:02:15 -0500

    Adding home page in mobile view, fixing navigation bar measures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <NavBar />;
+      <NavBar />
+      <Home />
+     
     </>
   );
 }
