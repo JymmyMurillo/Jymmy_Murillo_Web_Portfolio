19d0be557d20bca1e21ee06d6e19460f3a0fa7bf
Jymmy Murillo <murillojymmy@gmail.com>
2022-04-07 19:18:10 -0500

    Adding about me page

--- src/components/About Me.jsx
+++ src/components/About Me.jsx
@@ -1,7 +1,18 @@
 function AboutMe() {
   return (
     <div className="own-divAbout">
-      <div></div>
+      <div className="own-divContent d-flex flex-column justify-content-around">
+        <h2 className="own-h2About m-0">Acerca de Mí</h2>
+        <p className="own-pAbout m-0">
+          Soy un desarrollador full stack ubicado en Tunja-Boyacá. Tengo una
+          gran pasión por los efectos de interfaz de usuario, las animaciones y
+          la creación de experiencias de usuario intuitivas y dinámicas. <br></br>
+          Interesado en todo el espectro frontend y backend, trabajando en
+          proyectos ambiciosos con personas positivas.
+        </p>
+        <a href="x" className="own-aAbout">Aquí puedes ver mi CV</a>
+      </div>
+
       <span className="own-scroll-down-left d-flex flex-row justify-content-between">
         {`<-`} Scroll Up or Down {`->`}
       </span>
