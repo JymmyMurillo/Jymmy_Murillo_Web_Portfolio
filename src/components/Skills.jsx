25752382eff4a520fc95d22a0e9c2e4fe39bdc2c
Jymmy Murillo <murillojymmy@gmail.com>
2022-07-12 17:25:33 -0500

    Adding stack of technologies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,14 +8,219 @@ function Skills() {
           medio del uso de las siguintes tecnologias:
         </p>
         <div className="own-divTecnologies d-flex flex-wrap justify-content-center p-2">
-          <img src="./images/html5.png" alt="Logo html" className="own-logosSkills"/>
-          <img src="./images/js.png" alt="Logo javascript" className="own-logosSkills"/>
-          <img src="./images/css.png" alt="Logo css" className="own-logosSkills"/>
-          <img src="./images/1200px-React.jpg" alt="Logo React" className="own-logosSkills"/>
-          <img src="./images/php.png" alt="Logo php" className="own-logosSkills"/>
-          <img src="./images/Bootstrap.jpg" alt="Logo Bootstrap" className="own-logosSkills"/>
-          <img src="./images/git.png" alt="Logo git" className="own-logosSkills"/>
-          <img src="./images/github.png" alt="Logo github" className="own-logosSkills"/>
+          <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+              alt="bootstrap"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://www.w3schools.com/css/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://www.djangoproject.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://profilinator.rishav.dev/skills-assets/django-original.svg"
+              alt="django"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
+              alt="figma"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://firebase.google.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+              alt="firebase"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+              alt="git"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://heroku.com" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+              alt="heroku"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html5"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://www.adobe.com/in/products/illustrator.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg"
+              alt="illustrator"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://laravel.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain-wordmark.svg"
+              alt="laravel"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.linux.org/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
+              alt="linux"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://mariadb.org/" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg"
+              alt="mariadb"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.mathworks.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png"
+              alt="matlab"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="mongodb"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+              alt="mysql"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.php.net" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"
+              alt="php"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+              alt="postgresql"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://postman.com" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+              alt="postman"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.python.org" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+              alt="python"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://sass-lang.com" target="_blank" rel="noreferrer">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
+              alt="sass"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://www.sqlite.org/" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg"
+              alt="sqlite"
+              className="own-logosSkills"
+            />
+          </a>
+          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+            <img
+              src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+              alt="tailwind"
+              className="own-logosSkills"
+            />
+          </a>
+          <a
+            href="https://www.apachefriends.org/es/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://profilinator.rishav.dev/skills-assets/xampp.png"
+              alt="XAMPP"
+              className="own-logosSkills"
+            />
+          </a>
         </div>
       </div>
 
@@ -29,4 +234,4 @@ function Skills() {
   );
 }
 
-export default Skills
+export default Skills;
