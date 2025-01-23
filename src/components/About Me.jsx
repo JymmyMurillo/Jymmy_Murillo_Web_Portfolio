b6580514eb4f3125e9162c1107f022799f149437
Jymmy Murillo <murillojymmy@gmail.com>
2025-01-22 23:39:53 -0500

    Changing description in ABOUT ME section

--- src/components/About Me.jsx
+++ src/components/About Me.jsx
@@ -6,12 +6,19 @@ function AboutMe() {
       <div className="own-divContent d-flex flex-column justify-content-around">
         <h2 className="own-h2About m-0">Acerca de Mí</h2>
         <p className="own-pAbout m-0">
-          Soy un desarrollador full stack ubicado en Tunja-Boyacá. Tengo una
-          gran pasión por los efectos de interfaz de usuario, las animaciones y
-          la creación de experiencias de usuario intuitivas y dinámicas.{" "}
+          ¡Hola! Soy un ingeniero en mecatrónica apasionado por la tecnología en
+          todas sus formas. He tenido la oportunidad de crear soluciones de
+          automatización para sistemas físicos y digitales en diferentes áreas.
+          También he sido formador, compartiendo lo que sé y aprendiendo
+          muchísimo de mis alumnos en el camino. ¡Siempre hay algo nuevo por
+          descubrir en este inmenso y emocionante mundo de la tecnología!
           <br></br>
-          Interesado en todo el espectro frontend y backend, trabajando en
-          proyectos ambiciosos con personas positivas.
+          <br></br>
+          Si quieres saber más sobre mí, sigue explorando mi página web. Estoy
+          aquí para ayudarte con el desarrollo tecnológico que necesitas. No
+          dudes en contactarme, estaré encantado de escuchar tus ideas y
+          trabajar contigo para convertirlas en realidad. ¡Hagamos equipo y
+          logremos algo increíble juntos!
         </p>
         <a
           href="./hv/HV_JYMMY MURILLO.pdf"
